065f4423081720ca9b3466d8b4dd3872716b815a
Jozef-Bodorik <bodorikjozef11@gmail.com>
2026-01-03 01:36:06 +0100

    test

--- azure-demo/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "private_key_pem" {
-  value     = tls_private_key.example_ssh.private_key_pem
-  sensitive = true
-}
--- my-playground/app/simple-app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.get("/")
-def home():
-    return {"message": "hello kubernetes"}
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
